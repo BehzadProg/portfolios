1abef9d59cbf892b1722dfcd1384c464626e8c41
Behzad Rouhani <behxad.rowhani80.official@gmail.com>
2023-10-11 16:03:06 +0330

    setup blog&blog-details&portfolio-details template

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontend.home');
 });
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+Route::get('/blog-details', function () {
+    return view('frontend.blog_details');
+});
+Route::get('/portfolio-details', function () {
+    return view('frontend.portfolio_details');
+});
 
 Route::get('/dashboard', [DashboardController::class , 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
