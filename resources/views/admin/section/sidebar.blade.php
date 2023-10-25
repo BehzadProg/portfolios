3ebe6f019fafd61e06b3a84e2b66ef3faa459c78
Behzad Rouhani <behxad.rowhani80.official@gmail.com>
2023-10-25 16:47:06 +0330

    finishing our project by deleting unnecssary files and bulding vite server completly

--- resources/views/admin/section/sidebar.blade.php
+++ resources/views/admin/section/sidebar.blade.php
@@ -6,12 +6,8 @@
       <img alt="image" src="{{asset('assets/img/avatar/avatar-1.png')}}" class="rounded-circle mr-1">
       <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
       <div class="dropdown-menu dropdown-menu-right">
-        <div class="dropdown-title">Logged in 5 min ago</div>
         <a href="{{route('profile.edit')}}" class="dropdown-item has-icon">
           <i class="far fa-user"></i> Profile
-        </a>
-        <a href="features-settings.html" class="dropdown-item has-icon">
-          <i class="fas fa-cog"></i> Settings
         </a>
         <div class="dropdown-divider"></div>
 
--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg main_menu" id="main_menu_area">
     <div class="container">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="{{route('home.page')}}">
             <img src="{{asset(env('GENERAL_SETTING_IMAGE_UPLOAD_PATH').$generalSetting->logo)}}" alt="Rabins">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -10,8 +10,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#home-page">Home</a>
+                    <a class="nav-link" aria-current="page" href="{{Route::currentRouteName() == 'home.page' ? '#home-page' : url('/')}}">Home</a>
                 </li>
+                @if (Route::currentRouteName() == 'home.page')
+
                 <li class="nav-item">
                     <a class="nav-link" href="#about-page">About</a>
                 </li>
@@ -30,6 +32,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#contact-page">Contact</a>
                 </li>
+                @endif
             </ul>
         </div>
     </div>
